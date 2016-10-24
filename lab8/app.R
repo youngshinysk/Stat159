433cdff9a3101d92c68ace72c2f96be372ae44eb
Youngshin Kim <youngshinysk@gmail.com>
2016-10-24 10:54:54 -0700

    lab8

--- lab8/app.R
+++ lab8/app.R
@@ -1,5 +1,21 @@
 library(babynames)
 library(ggplot2)
 library(stringr)
+library(shiny)
 
-asdf
+adv = read.csv('advertising.csv')
+
+ui <- fluidPage(
+  selectInput(inputId = "feature",
+              label = "Choose a feature",
+              choices = c('TV', 'Radio', 'Newspaper')),
+  plotOutput(outputId = "plot")
+)
+
+server <- function(input, output) {
+  output$plot <- renderPlot({
+    plot(adv$Sales, adv[,input$feature], xlab = input$feature, ylab = 'Sales', main = paste('Sales vs.', input$feature))
+  })
+}
+
+shinyApp(server = server, ui = ui)
